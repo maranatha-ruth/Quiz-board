12ae09f459aedab47f2d222b77daa11bd26b774f
maranatha-ruth <maranatharuth6@gmail.com>
2018-12-21 15:55:45 +0200

    arrange indentation

--- js/script.js
+++ js/script.js
@@ -10,12 +10,27 @@ var testResults = function(one, two ,three,four){
       });
     
       $("form#test").submit(function(event){
-        var Q1 = parseInt($("input:radio[name=question1]:checked").val());
-        var Q2 = parseInt($("input:radio[name=question2]:checked").val());
-        var Q3 = parseInt($("input:radio[name=question3]:checked").val());
-        var Q4 = parseInt($("input:radio[name=question4]:checked").val());
-    
-        var result = testResults(Q1, Q2 ,Q3,Q4);
+        var Q1 = parseInt($("input:radio[name=question one]:checked").val());
+        var Q2 = parseInt($("input:radio[name=question one]:checked").val());
+        var Q3 = parseInt($("input:radio[name=question three]:checked").val());
+        var Q4 = parseInt($("input:radio[name=question four]:checked").val());
+         
+        var score =0;
+        
+        if (questionone===+10){
+            score +=10;
+        }
+        if (questionTwo===+10){
+            score +=10;
+
+        }
+        if (questionThree===+10){
+            score +=10;
+        }
+        if (questionFour===+10){
+            score +=10;
+        }
+
     
         $("#display").text("Your score is:" + result);
     
